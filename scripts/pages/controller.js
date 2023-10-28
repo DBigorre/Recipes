d7b2c298077e64f6b4927bdf9e4717170ef1b0b5
desiree bigorre <bigorredesiree@gmail.com>
2023-10-28 15:18:36 -0400

    css et debut des filtres

--- scripts/pages/controller.js
+++ scripts/pages/controller.js
@@ -18,3 +18,40 @@ async function catchIngredients(recette){
   }*/
   ingredientsListByRecipe(listOfIngredients, recette);
 };
+
+//recuperer la liste de tous les ingredients
+async function recoveryListOfAll(){
+  const model = new Model();
+  const datas = await model.getDatas();
+  const recipes = await datas.recipes;
+
+  let allIngredients = [];
+  let allAppliances = [];
+  let allTools= [];
+
+  for (let recipe of recipes){
+    for ( let ingredient of recipe.ingredients){
+      allIngredients.push(ingredient.ingredient)
+    }
+    for (let ustensil of recipe.ustensils){
+      allTools.push(ustensil)
+    }
+    allAppliances.push(recipe.appliance)
+  }
+  const filteredArrayIngredients = allIngredients.filter(function(ele, pos) {
+   return allIngredients.indexOf(ele) == pos;
+  })
+  const filteredArrayAppliances = allAppliances.filter(function(ele, pos) {
+    return allAppliances.indexOf(ele) == pos;
+   })
+   const filteredArrayTools = allTools.filter(function(ele, pos) {
+    return allTools.indexOf(ele) == pos;
+   })
+
+
+let filteredArrayOfAllIngredients = filteredArrayIngredients.sort();
+let filteredArrayOfAllAppliances = filteredArrayAppliances.sort();
+let filteredArrayOfAllTools = filteredArrayTools.sort();
+
+fillListOfFilters(filteredArrayOfAllIngredients, filteredArrayOfAllAppliances, filteredArrayOfAllTools)
+}
--- scripts/pages/views/index.js
+++ scripts/pages/views/index.js
@@ -8,11 +8,13 @@ function displayListRecipes(recipes){
       <div>
         <article>
           <img src="assets/images/${recipe.image}" alt="Photo de la recette" class="recipePhoto">
-          <h2> ${recipe.name}</h2>
-          <h3> Recettes </h3>
-          <p> ${recipe.description} </p>
-          <h3> Ingredients </h3>
-          <div class="recipesCardsIngredient" id="${recipe.name}">
+          <div class="recipeCardInfo">
+            <h2> ${recipe.name}</h2>
+            <h3> Recettes </h3>
+            <p> ${recipe.description} </p>
+            <h3> Ingredients </h3>
+            <div class="recipesCardsIngredient" id="${recipe.name}">
+            </div>
           </div>
         </article>
       </div>
@@ -27,12 +29,93 @@ function ingredientsListByRecipe(listOfIngredients, recette){
   let ingredientsHtml = "";
 
   for (let components of listOfIngredients){
+    if(components.unit == undefined){
+      components.unit = " "
+    };
+    if(components.quantity == undefined){
+      components.quantity = " "
+    };
       ingredientsHtml += `
         <ul>
           <li> ${components.ingredient} </li>
+          <li class="componentsQuantity"> ${components.quantity} ${components.unit}</li>
         </ul>
       `
     }
   let htmlIngredients = document.getElementById(recette.name)
   htmlIngredients.innerHTML = ingredientsHtml
 }
+
+// filter
+function fillListOfFilters(filteredArrayOfAllIngredients, filteredArrayOfAllAppliances, filteredArrayOfAllTools){
+  let listOfFilterIngredients = document.getElementById("ingredientsList");
+  let listOfFilterDevice = document.getElementById("deviceList");
+  let listOfFilterTools = document.getElementById("toolsList");
+
+  let ingredientsListHtml = "";
+  let deviceListHtml = "";
+  let toolsListHtml = "";
+
+  for (let i of filteredArrayOfAllIngredients){
+    ingredientsListHtml += `
+      <ul>
+        <li> ${i} </li>
+      </ul>
+    `
+  };
+  listOfFilterIngredients.innerHTML = ingredientsListHtml
+
+  for (let i of filteredArrayOfAllAppliances){
+    deviceListHtml += `
+      <ul>
+        <li> ${i} </li>
+      </ul>
+    `
+  };
+  listOfFilterDevice.innerHTML = deviceListHtml
+  for (let i of filteredArrayOfAllTools){
+    toolsListHtml += `
+      <ul>
+        <li> ${i} </li>
+      </ul>
+    `
+  };
+  listOfFilterTools.innerHTML = toolsListHtml
+  listenerForFilters()
+};
+
+function listenerForFilters(){
+  let ingredientsButton = document.getElementById("ingredientsfilter")
+  let deviceButton = document.getElementById("devicefilter")
+  let toolsButton = document.getElementById("toolsfilter")
+
+  ingredientsButton.addEventListener("click", function (event){
+    displayIngredientsList()
+  });
+  deviceButton.addEventListener("click", function (event){
+    displayDevicesList()
+  });
+  toolsButton.addEventListener("click", function (event){
+    displayToolsList()
+  });
+}
+
+function displayIngredientsList(){
+  let ingredientList = document.getElementById("ingredientsList")
+  let ingredientSearch = document.getElementById("searchIngredients")
+  ingredientList.style.display = "block"
+  ingredientSearch.style.display = "block"
+}
+
+function displayDevicesList(){
+  let deviceList = document.getElementById("deviceList")
+  let deviceSearch = document.getElementById("searchDevices")
+  deviceList.style.display = "block"
+  deviceSearch.style.display = "block"
+}
+function displayToolsList() {
+  let toolsList = document.getElementById("toolsList")
+  let toolSearch = document.getElementById("searchTools")
+  toolsList.style.display = "block"
+  toolSearch.style.display = "block"
+  }
