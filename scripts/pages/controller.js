8ec311e06cd87c7a8043f20148eeb6e47c291701
desiree bigorre <bigorredesiree@gmail.com>
2023-11-07 12:16:43 -0500

    filtres fonctionnels et tag filtres actifs

--- scripts/pages/controller.js
+++ scripts/pages/controller.js
@@ -12,44 +12,6 @@ function displaySelectList(recipes){
 
   let allList = recoveryListOfAll(recipes);
   fillListOfFilters(allList.filteredArrayOfAllIngredients, allList.filteredArrayOfAllAppliances, allList.filteredArrayOfAllTools);
-  listenerForFilters()
+  listenerForFilters();
+  stockerEtCacherLesFiltresActifs(allList.filteredArrayOfAllIngredients, allList.filteredArrayOfAllAppliances, allList.filteredArrayOfAllTools)
 };
-
-//recuperer la liste de tous les ingredients
-function recoveryListOfAll(recipes){
-
-  let allIngredients = [];
-  let allAppliances = [];
-  let allTools= [];
-
-  for (let recipe of recipes){
-    for ( let ingredient of recipe.ingredients){
-      allIngredients.push(ingredient.ingredient)
-    }
-    for (let ustensil of recipe.ustensils){
-      allTools.push(ustensil)
-    }
-    allAppliances.push(recipe.appliance)
-  }
-  const filteredArrayIngredients = allIngredients.filter(function(ele, pos) {
-   return allIngredients.indexOf(ele) == pos;
-  })
-  const filteredArrayAppliances = allAppliances.filter(function(ele, pos) {
-    return allAppliances.indexOf(ele) == pos;
-  })
-  const filteredArrayTools = allTools.filter(function(ele, pos) {
-    return allTools.indexOf(ele) == pos;
-  })
-
-
-  let filteredArrayOfAllIngredients = filteredArrayIngredients.sort();
-  let filteredArrayOfAllAppliances = filteredArrayAppliances.sort();
-  let filteredArrayOfAllTools = filteredArrayTools.sort();
-
-  return({
-    filteredArrayOfAllIngredients,
-    filteredArrayOfAllAppliances,
-    filteredArrayOfAllTools
-  })
-
-}
--- scripts/pages/views/index.js
+++ scripts/pages/views/index.js
@@ -1,5 +1,12 @@
+let arrayOfUserIngredientSelection = [];
+let arrayOfUserDeviceSelection = [];
+let arrayOfUserToolSelection = [];
+// variable globale, liste des recettes que la vue est en train d'afficher
+let listRecipes = null;
+
 function displayListRecipes(recipes){
   let html= "";
+  listRecipes = recipes;
 
   for (let recipe of recipes) {
     let htmlIngredients = getHtmlIngredientsForCard(recipe);
@@ -59,26 +66,20 @@ function fillListOfFilters(filteredArrayOfAllIngredients, filteredArrayOfAllAppl
 
   for (let i of filteredArrayOfAllIngredients){
     ingredientsListHtml += `
-      <ul>
         <li class="ingredientDeLaListe"> ${i} </li>
-      </ul>
     `
   };
   listOfFilterIngredients.innerHTML = ingredientsListHtml
 
   for (let i of filteredArrayOfAllAppliances){
     deviceListHtml += `
-      <ul>
         <li class="deviceDeLaListe"> ${i} </li>
-      </ul>
     `
   };
   listOfFilterDevice.innerHTML = deviceListHtml
   for (let i of filteredArrayOfAllTools){
     toolsListHtml += `
-      <ul>
-        <li> ${i} </li>
-      </ul>
+        <li class="toolDeLaListe"> ${i} </li>
     `
   };
   listOfFilterTools.innerHTML = toolsListHtml
@@ -101,11 +102,13 @@ function listenerForFilters(){
 }
 
 function displayIngredientsList(){
+
   let ingredientList = document.getElementById("ingredientsList")
   let ingredientSearch = document.getElementById("searchIngredients")
   ingredientList.style.display = "block"
   ingredientSearch.style.display = "block"
   listenOfIngredientsInput()
+  listenAndStockIngredients()
 }
 
 function displayDevicesList(){
@@ -120,106 +123,237 @@ function displayToolsList() {
   let toolSearch = document.getElementById("searchTools")
   toolsList.style.display = "block"
   toolSearch.style.display = "block"
+  listenOfToolInput()
 }
 
 //fonction recheche dans input list of ingredient
 function listenOfIngredientsInput(){
   let input = document.getElementById("searchIngredients");
-  let userWordArray = [];
   let userWord = ""
 
-  input.addEventListener("keypress", function (event){
-      userWordArray.push(event.key)
-      userWord = userWordArray.join("")
+  input.addEventListener("keyup", function (event){
+      userWord = event.target.value
+      console.log(userWord)
       // fonction de tri qui renvoie uniquement les bons ingredients
       trierIngredientsInList(userWord)
   })
-  /*input.addEventListener("keydown", function(event){
-    if (event.key == "Backspace") {
-      userWordArray.pop()
-      trierIngredientsInList(userWord)
-    }
-  })*/
 }
 
 function trierIngredientsInList(userWord) {
-  let ingredients = document.querySelectorAll(".ingredientDeLaListe")
-  let ingredientArrayFilteredWithUserWord = [];
+  let ingredients = document.querySelectorAll(".ingredientDeLaListe");
 
   for (let ingredient of ingredients){
     let nameOfIngredient = ingredient.textContent
     if(nameOfIngredient.includes(userWord)){
-      ingredientArrayFilteredWithUserWord.push(nameOfIngredient)
+      ingredient.classList.remove("hide")
+    } else{
+      ingredient.classList.add("hide")
     }
   }
-  afficherLaListeDesIngredientsFilteeAvecLeUserWord(ingredientArrayFilteredWithUserWord)
 }
 
-function afficherLaListeDesIngredientsFilteeAvecLeUserWord(ingredientArrayFilteredWithUserWord){
-  let listOfFilterIngredients = document.getElementById("ingredientsList");
-  let ingredientsListHtml = "";
-
-  for (let i of ingredientArrayFilteredWithUserWord){
-    ingredientsListHtml += `
-      <ul>
-        <li class="ingredientDeLaListe"> ${i} </li>
-      </ul>
-    `
-  };
-  listOfFilterIngredients.innerHTML = ingredientsListHtml
-};
 
 //fonction recherche dans input liste des devices
 function listenOfDeviceInput(){
   let input = document.getElementById("searchDevices");
-  let userWordArray = [];
   let userWord = ""
 
-  input.addEventListener("keypress", function (event){
-    userWordArray.push(event.key)
-    userWord = userWordArray.join("")
+  input.addEventListener("keyup", function (event){
+    userWord = event.target.value
 
     trierDevicesInList(userWord)
   })
-  /*input.addEventListener("keydown", function(event){
-    if (event.key == "Backspace") {
-      userWordArray.pop()
-      trierDevicesInList(userWord)
-    }
-  })*/
 }
 
 function trierDevicesInList(userWord) {
   let devices = document.querySelectorAll(".deviceDeLaListe")
-  let deviceArrayFilteredWithUserWord = [];
 
   for (let device of devices){
     let nameOfDevice = device.textContent
     if(nameOfDevice.includes(userWord)){
-      deviceArrayFilteredWithUserWord.push(nameOfDevice)
+      device.classList.remove("hide")
+    }else{
+      device.classList.add("hide")
     }
   }
-  afficherLaListeDesDevicesFiltreeAvecLeUserWord(deviceArrayFilteredWithUserWord)
 }
 
-function afficherLaListeDesDevicesFiltreeAvecLeUserWord(deviceArrayFilteredWithUserWord){
-  let listOfFilterDevice = document.getElementById("deviceList");
-  let deviceListHtml = "";
-
-  for (let i of deviceArrayFilteredWithUserWord){
-    deviceListHtml += `
-      <ul>
-        <li class="deviceDeLaListe"> ${i} </li>
-      </ul>
-    `
-  };
-  listOfFilterDevice.innerHTML = deviceListHtml
-};
-
-
-
 //fonction recherche dans input liste des tools
+function listenOfToolInput(){
+  let input = document.getElementById("searchTools");
+  let userWord = ""
+
+  input.addEventListener("keypress", function (event){
+    userWord = event.target.value
+    trierToolsInList(userWord)
+  })
+}
+
+function trierToolsInList(userWord) {
+  let tools = document.querySelectorAll(".toolDeLaListe")
+
+  for (let tool of tools){
+    let nameOfTool = tool.textContent
+    if(nameOfTool.includes(userWord)){
+      tool.classList.remove("hide")
+    }else{
+      tool.classList.add("hide")
+    }
+  }
+}
+
+//Ecoute et stockage des ingredients, devices et tools
+function listenAndStockIngredients(){
+  let listOfIngredients = document.querySelectorAll(".ingredientDeLaListe");
+  let listOfDevices = document.querySelectorAll(".deviceDeLaListe");
+  let listOfTools = document.querySelectorAll(".toolDeLaListe");
+
+  for(let ingredient of listOfIngredients){
+    ingredient.addEventListener("click", function(event){
+      arrayOfUserIngredientSelection.push(ingredient.textContent)
+      displayFiltersActifs(arrayOfUserIngredientSelection, arrayOfUserDeviceSelection, arrayOfUserToolSelection)
+    })
+  }
+  for(let device of listOfDevices){
+    device.addEventListener("click", function(event){
+      arrayOfUserDeviceSelection.push(device.textContent)
+      displayFiltersActifs(arrayOfUserIngredientSelection, arrayOfUserDeviceSelection, arrayOfUserToolSelection)
+    })
+  }
+  for(let tool of listOfTools){
+    tool.addEventListener("click", function(event){
+      arrayOfUserToolSelection.push(tool.textContent)
+      displayFiltersActifs(arrayOfUserIngredientSelection, arrayOfUserDeviceSelection, arrayOfUserToolSelection)
+    })
+  }
+  //displayFiltersActifs(arrayOfUserIngredientSelection)
+
+}
+
+/*function displayFiltersActifs(arrayOfUserIngredientSelection, arrayOfUserDeviceSelection, arrayOfUserToolSelection){
+  let filtersForYellowDisplay = document.querySelector(".filtersActifs")
+  let yellowsFiltersHtml = "";
+  for (let i of arrayOfUserIngredientSelection){
+    yellowsFiltersHtml += `
+    <button class="yellowFilter"> ${i} <i class="fa-solid fa-xmark"></i></button>
+    `
+  }
+  for (let i of arrayOfUserDeviceSelection){
+    yellowsFiltersHtml += `
+    <button class="yellowFilter"> ${i} <i class="fa-solid fa-xmark"></i></button>
+    `
+  }
+  for (let i of arrayOfUserToolSelection){
+    yellowsFiltersHtml += `
+    <button class="yellowFilter"> ${i} <i class="fa-solid fa-xmark"></i></button>
+    `
+  }
+  filtersForYellowDisplay.innerHTML = yellowsFiltersHtml
+  removeFiltersActifs()
+}*/
+
+function stockerEtCacherLesFiltresActifs(filteredArrayOfAllIngredients, filteredArrayOfAllAppliances, filteredArrayOfAllTools){
+  let filtersForYellowDisplay = document.querySelector(".filtersActifs")
+  let yellowsFiltersHtml = "";
+
+  for (let ingredient of filteredArrayOfAllIngredients){
+    yellowsFiltersHtml += `
+      <button class="yellowFilter hide"> ${ingredient} <i class="fa-solid fa-xmark"></i></button>
+    `
+  }
+  for (let device of filteredArrayOfAllAppliances){
+    yellowsFiltersHtml += `
+      <button class="yellowFilter hide"> ${device} <i class="fa-solid fa-xmark"></i></button>
+    `
+  }
+  for (let tool of filteredArrayOfAllTools){
+    yellowsFiltersHtml += `
+      <button class="yellowFilter hide"> ${tool} <i class="fa-solid fa-xmark"></i></button>
+    `
+  }
+
+  filtersForYellowDisplay.innerHTML = yellowsFiltersHtml
+}
+
+function displayFiltersActifs(arrayOfUserIngredientSelection, arrayOfUserDeviceSelection, arrayOfUserToolSelection){
+  let filtersForYellowDisplay = document.querySelectorAll(".filtersActifs")
+  let filtersHide = document.querySelectorAll(".yellowFilter")
+  let yellowsFiltersHtml = "";
+
+  for(let filter of filtersHide){
+    for(let ingredient of arrayOfUserIngredientSelection){
+      if(filter.textContent == ingredient){
+        filter.classList.remove("hide")
+      }
+    }
+    for(let device of arrayOfUserDeviceSelection){
+      if(filter.textContent == device){
+        filter.classList.remove("hide")
+      }
+    }
+    for(let tool of arrayOfUserToolSelection){
+      if(filter.textContent == tool){
+        filter.classList.remove("hide")
+      }
+    }
+  }
+
+  filtersForYellowDisplay.innerHTML = yellowsFiltersHtml
+  removeFiltersActifs()
+
+
+
+  /*for (let i of arrayOfUserDeviceSelection){
+    yellowsFiltersHtml += `
+    <button class="yellowFilter"> ${i} <i class="fa-solid fa-xmark"></i></button>
+    `
+  }
+  for (let i of arrayOfUserToolSelection){
+    yellowsFiltersHtml += `
+    <button class="yellowFilter"> ${i} <i class="fa-solid fa-xmark"></i></button>
+    `
+  }*/
+}
+
+// fonction de suppression des filtres si clic sur la croix
+function removeFiltersActifs(){
+  let filtersActif = document.querySelectorAll(".yellowFilter");
+
+  /*for(let filterActif of filtersActif){
+    if (!filterActif.classList.contains("hide")){
+      filterActif.addEventListener("click", function(event) {
+        console.log(filterActif)
+        filterActif.classList.add("hide")
+      })
+    }
+  }
+
+  for(let filterActif of filtersActif){
+    let filterActifClose = filterActif.querySelector(".fa-xmark")
+    filterActifClose.addEventListener("click", function(event) {
+      console.log(filterActif)
+      filterActif.classList.add("hide")
+    })
+  }*/
+  //let filterActifClose = document.querySelectorAll(".fa-xmark");
+  //for (let filterActif of filtersActif){
+    //console.log(filterActif)
+    //filterActifClose.addEventListener("click", function(event) {
+      //filterActif.classList.add("hide")
+    //})
+  //}
+}
+
+// fonction pour le triage des recettes
+// grosse fonction des filtres
+
+
+
 
 // problemes:
-// bouton suppr
-// tableau vide bug
+//lorsque je reselectionne un filtre après une suppression, tous les filtres supprimés reviennent
+
+
+//a faire:
+// refermer la liste (rajouter un classe open a l'ouverture et si reclick alors que la liste est ouverte faire un display none)
+// supprimer la possibilité de douclons dans les filtres
